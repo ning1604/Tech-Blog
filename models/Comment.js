67d297f9407052e258f1dcbd3f99561426f3d1ad
Ning <100749559+ning1604@users.noreply.github.com>
2022-09-07 16:59:15 +1000

    fixed application not running and some code clean up

--- models/Comment.js
+++ models/Comment.js
@@ -42,4 +42,6 @@ Comment.init(
         underscored: true,
         modelName: 'comment',
     }
-)
+);
+
+module.exports = Comment;
--- models/index.js
+++ models/index.js
@@ -20,16 +20,16 @@ Post.belongsTo(User, {
 Post.hasMany(Comment, {
   foreignKey: 'post_id',
   onDelete: 'CASCADE'
-})
+});
 
 Comment.belongsTo(Post, {
   foreignKey: 'post_id',
   onDelete: 'CASCADE'
-})
+});
 
 Comment.belongsTo(User, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
-})
+});
 
 module.exports = { User, Post, Comment };
