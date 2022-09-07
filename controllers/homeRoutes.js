5b1e163e6aeff498f49b8dae4fd9bb32201063a9
Ning <100749559+ning1604@users.noreply.github.com>
2022-09-07 16:37:46 +1000

    setup server.js and added homepageRoute endpoint for testing

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const { User, Post, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get("/", async (req, res) => {
+    try {
+        res.render("homepage", {
+            logged_in: req.session.logged_in,
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
