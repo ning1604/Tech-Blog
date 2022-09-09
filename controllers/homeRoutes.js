5088564d89b0b694c73d2d03f0408259d1bed118
Ning <100749559+ning1604@users.noreply.github.com>
2022-09-09 17:32:37 +1000

    added login and sign up functionality

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -49,4 +49,27 @@ router.get('/signup', (req, res) => {
     res.render('signup');
 })
 
+// user dashboard routes with 'withAuth' to prevent access to route
+router.get("/dashboard", withAuth, async (req, res) => {
+    try {
+        // Find the logged in user based on the session ID
+        if (!req.session.logged_in) {
+            res.redirect("/login");
+        }
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ["password"] },
+            include: [{ model: Post }],
+        });
+
+        const user = userData.get({ plain: true });
+
+        res.render("dashboard", {
+            ...user,
+            logged_in: true,
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
