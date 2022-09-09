18e85e81aed5bd0579fb4a9c9f324f5d93dd246d
Ning <100749559+ning1604@users.noreply.github.com>
2022-09-09 14:45:02 +1000

    added login and sign up pages to application

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const { User, Post, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
+// home routes
 router.get("/", async (req, res) => {
     try {
         // gets all posts and JOIN with user data
@@ -26,5 +27,26 @@ router.get("/", async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+// login and sign up routes
+router.get('/login', (req, res) => {
+    // if the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+        res.redirect('/dashboard');
+        return;
+    }
+
+    res.render('login');
+})
+
+router.get('/signup', (req, res) => {
+    // if the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+        res.redirect('/dashboard');
+        return;
+    }
+
+    res.render('signup');
+})
 
 module.exports = router;
