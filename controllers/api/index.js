d946f4eab48ddaeb9eee4b27adaf14e0712f027b
Ning <100749559+ning1604@users.noreply.github.com>
2022-09-11 14:27:02 +1000

    added create new post functionality

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-// const dashboardRoutes = require('./dashboardRoutes');
+const postRoutes = require('./postRoutes');
 
 router.use('/users', userRoutes);
-// router.use('/dashboard', dashboardRoutes);
+router.use('/posts', postRoutes);
 
 module.exports = router;
--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,28 +1,28 @@
-// const newPostFormHandler = async (event) => {
-//     event.preventDefault();
+const newPostFormHandler = async (event) => {
+    event.preventDefault();
 
-//     // collect values from the create post form
-//     const title = document.querySelector('#post-title').value.trim();
-//     const content = document.querySelector('#post-content').value.trim();
+    // collect values from the create post form
+    const title = document.querySelector('#post-title').value.trim();
+    const content = document.querySelector('#post-content').value.trim();
 
-//     if (title && content) {
-//         const response = await fetch('/api/posts', {
-//             method: 'POST',
-//             body: JSON.stringify({ title, content }),
-//             headers: { 'Content-Type': 'application/json' },
-//         });
+    if (title && content) {
+        const response = await fetch('/api/posts', {
+            method: 'POST',
+            body: JSON.stringify({ title, content }),
+            headers: { 'Content-Type': 'application/json' },
+        });
 
-//         if (response.ok) {
-//             document.location.replace('/dashboard');
-//         } else {
-//             alert('Failed to create post');
-//         }
-//     }
-// };
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Failed to create post');
+        }
+    }
+};
 
-// document
-//     .querySelector('.new-post-form')
-//     .addEventListener('submit', newPostFormHandler);
+document
+    .querySelector('.new-post-form')
+    .addEventListener('submit', newPostFormHandler);
 
 // event listenter to unhide create new post form and hide existing post
 const newPostBtn = document.querySelector('.new-post-btn');
